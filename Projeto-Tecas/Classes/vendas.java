97f53849f11b8badaeb22832228aa42fffc45eff
Sarah Foster <102382880+sarahfosteer@users.noreply.github.com>
2022-03-27 18:31:15 -0300

    Create vendas.java
    Classe principal de todo o projeto

---
+++ Projeto-Tecas/Classes/vendas.java
@@ -0,0 +1,18 @@
+
+package classes;
+
+import frm.frm_login;
+
+/**
+ *
+ * @author Sarah Foster
+ */
+public class vendas {
+    public static void main(String[] args){
+    
+    frm_login my_login = new frm_login();
+    my_login.setLocationRelativeTo(null);
+    my_login.setVisible(true);
+    
+    }
+}
